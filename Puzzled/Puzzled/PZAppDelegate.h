fefd868f11095fa1f11ba4500d477bf6b91c157c
Ostap Horbach <mail.ostapa@gmail.com>
2014-05-06 12:20:16 +0300

    Initial project

---
+++ Puzzled/Puzzled/PZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PZAppDelegate.h
+//  Puzzled
+//
+//  Created by Ostap Horbach on 5/6/14.
+//  Copyright (c) 2014 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Puzzled/Puzzled/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Puzzled
+//
+//  Created by Ostap Horbach on 5/6/14.
+//  Copyright (c) 2014 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PZAppDelegate class]));
+    }
+}
